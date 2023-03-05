308c7417dc4e23c07e2b5f661c903611189a2140
Rumen Guin <rumen2k@gmail.com>
2023-03-06 00:47:17 +0530

    show youtube trailer for each movie

--- Netflix/Controllers/Core/HomeViewController.swift
+++ Netflix/Controllers/Core/HomeViewController.swift
@@ -77,6 +77,8 @@ extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
             
         }
         
+        cell.delegate = self
+        
         switch indexPath.section {
             
         case Sections.TrendingMovies.rawValue:
@@ -167,4 +169,15 @@ extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
         navigationController?.navigationBar.transform = .init(translationX: 0, y: min(0, -offset))
     }
     
+}
+
+extension HomeViewController: CollectionViewTableViewCellDelegate {
+    func collectionViewTableViewCellDidTapCell(_ cell: CollectionViewTableViewCell, viewModel: TitlePreviewViewModel) {
+        
+        DispatchQueue.main.async { [weak self] in
+            let vc = TitlePreviewViewController()
+            vc.configure(with: viewModel)
+            self?.navigationController?.pushViewController(vc, animated: true)
+        }
+    }
 }
