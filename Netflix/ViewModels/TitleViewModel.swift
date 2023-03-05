ad56aa4463acb5cde97fceb28cd0afbd245fe7d7
Rumen Guin <rumen2k@gmail.com>
2023-03-05 19:04:06 +0530

    create upcoming movie section

---
+++ Netflix/ViewModels/TitleViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  TitleViewModel.swift
+//  Netflix
+//
+//  Created by RUMEN GUIN on 05/03/23.
+//
+
+import Foundation
+
+struct TitleViewModel {
+    let titleName: String
+    let posterURL: String
+}
