0b25ddd4e98e1630464f7c057c28bb80b1b1b31c
Rumen Guin <rumen2k@gmail.com>
2023-03-05 21:55:25 +0530

    show searched movies

--- Netflix/Controllers/Core/SearchViewController.swift
+++ Netflix/Controllers/Core/SearchViewController.swift
@@ -44,6 +44,7 @@ class SearchViewController: UIViewController {
         navigationController?.navigationBar.tintColor = .systemRed
         
         fetchDiscoverMovies()
+        searchController.searchResultsUpdater = self
     }
     
     private func fetchDiscoverMovies() {
@@ -90,4 +91,30 @@ extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
         return 140
     }
     
+}
+
+extension SearchViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        let searchBar = searchController.searchBar
+        
+        guard let query = searchBar.text,
+              !query.trimmingCharacters(in: .whitespaces).isEmpty,
+              query.trimmingCharacters(in: .whitespaces).count >= 3,
+              let resultsController = searchController.searchResultsController as? SearchResultsViewController else {
+            return
+        }
+        
+        APICaller.shared.search(with: query) { result in
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let titles):
+                    resultsController.titles = titles
+                    resultsController.searchResultsCollectionView.reloadData()
+                case .failure(let error):
+                    print(error.localizedDescription)
+                }
+            }
+        }
+              
+    }
 }
